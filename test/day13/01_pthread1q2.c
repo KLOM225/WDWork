ce22b225c4d0bdc7438fd1bd2ce809d3d5ad5518
KLOM <573921802@qq.com>
2025-02-27 21:04:33 +0800

    2.27work

--- test/day13/01_pthread1q2.c
+++ test/day13/01_pthread1q2.c
@@ -13,11 +13,15 @@ int main(void){
     //创建三个线程 
     pthread_t thread1;
     pthread_t thread2;
-    pthread_t thread3;
     pthread_create(&thread1, NULL, pthread_main, NULL); 
     pthread_create(&thread2, NULL, pthread_main, NULL); 
-    pthread_create(&thread3, NULL, pthread_main, NULL); 
     //先阻塞5秒，让三个线程运行完
+    
+
+    for(int i = 0; i < 1000000; i++){
+        global_num++;
+    }
+
     sleep(5);
 
     printf("global_num = %d\n", global_num);
