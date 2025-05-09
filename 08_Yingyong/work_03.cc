9a2e81ba742826cc2bd3544e52ec55cc9487d312
KLOM <573921802@qq.com>
2025-05-09 01:16:01 +0000

    update 08_Yingyong/work_03.cc.

--- 08_Yingyong/work_03.cc
+++ 08_Yingyong/work_03.cc
@@ -12,12 +12,18 @@ pthread_cond_t cond;
 void *start_routine1(void *arg){
 
     pthread_mutex_lock(&mutex);
-    while(0 != flag){//将if换成while，是为了防止发生异常唤醒的情况
+    while(1 == flag ){//将if换成while，是为了防止发生异常唤醒的情况
         pthread_cond_wait(&cond, &mutex);
-    }
         
+    }
+    if(flag == 0){
     cout << "A" << endl;
     flag = 1;
+    }
+    if(flag == 2){
+    cout << "C" << endl;
+    flag = 0;
+    }
     pthread_mutex_unlock(&mutex);
     pthread_cond_signal(&cond);
     //return NULL;
@@ -28,7 +34,7 @@ void *start_routine1(void *arg){
 void *start_routine2(void *arg){
 
     pthread_mutex_lock(&mutex);
-    while(1 != flag){//将if换成while，是为了防止发生异常唤醒的情况
+    while(0 == flag){//将if换成while，是为了防止发生异常唤醒的情况
         pthread_cond_wait(&cond, &mutex);
     }
         
@@ -41,35 +47,35 @@ void *start_routine2(void *arg){
     
 }
 
-void *start_routine3(void *arg){
-
-    pthread_mutex_lock(&mutex);
-    while(2 != flag){//将if换成while，是为了防止发生异常唤醒的情况
-        pthread_cond_wait(&cond, &mutex);
-    }
-        
-    cout << "C" << endl;
-    flag = 0;
-    pthread_mutex_unlock(&mutex);
-    pthread_cond_signal(&cond);
-    //return NULL;
-    pthread_exit(NULL);//退出子线程
-    
-}
+//void *start_routine3(void *arg){
+//
+//    pthread_mutex_lock(&mutex);
+//    while(2 != flag){//将if换成while，是为了防止发生异常唤醒的情况
+//        pthread_cond_wait(&cond, &mutex);
+//    }
+//        
+//    cout << "C" << endl;
+//    flag = 0;
+//    pthread_mutex_unlock(&mutex);
+//    pthread_cond_signal(&cond);
+//    //return NULL;
+//    pthread_exit(NULL);//退出子线程
+//    
+//}
 int main(){
     pthread_mutex_init(&mutex, NULL);
     pthread_cond_init(&cond, NULL);
     
-    pthread_t pthid1, pthid2, pthid3;
+    pthread_t pthid1, pthid2;
     pthread_create(&pthid1, NULL, start_routine1, NULL);
     pthread_create(&pthid2, NULL, start_routine2, NULL);
-    pthread_create(&pthid3, NULL, start_routine3, NULL);
+    //pthread_create(&pthid3, NULL, start_routine3, NULL);
      
     cout << "pthread main id  : " << pthread_self() << endl;    
 
     pthread_join(pthid1, nullptr);
     pthread_join(pthid2, nullptr);
-    pthread_join(pthid3, nullptr);
+    //pthread_join(pthid3, nullptr);
     
     pthread_mutex_destroy(&mutex); 
     pthread_cond_destroy(&cond);
