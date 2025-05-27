9bc2e1f4e06f35eaf4e1e5cdd9b7b352a2251f1f
KLOM <573921802@qq.com>
2025-05-27 15:37:50 +0800

    02up

--- Projets/02/SmartHomeMonitorServer/src/main.cpp
+++ Projets/02/SmartHomeMonitorServer/src/main.cpp
@@ -4,8 +4,8 @@
 int main() {
     try {
         Configuration config;
-        std::cout << "Server IP: " << config.get_ip() << std::endl;
-        std::cout << "Port: " << config.get_port() << std::endl;
+        std::cout << "Server IP: " << config.GetIP() << std::endl;
+        std::cout << "Port: " << config.GetPort() << std::endl;
     } catch (const std::exception& e) {
         std::cerr << "Configuration error: " << e.what() << std::endl;
         return 1;
